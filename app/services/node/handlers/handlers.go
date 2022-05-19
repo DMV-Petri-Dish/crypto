38c9f4ce4f48bd097ce3aa8c9041003296fb06b0
john <jnkroeker@gmail.com>
2022-05-18 20:42:34 -0400

    tidy up app/services/node/main.go

--- app/services/node/handlers/handlers.go
+++ app/services/node/handlers/handlers.go
@@ -2,22 +2,90 @@
 package handlers
 
 import (
+	"context"
 	"expvar"
 	"net/http"
 	"net/http/pprof"
+	"os"
 
 	"github.com/DMV-Petri-Dish/crypto/app/services/node/handlers/debug/checkgrp"
+	"github.com/DMV-Petri-Dish/crypto/foundation/blockchain/state"
+	"github.com/DMV-Petri-Dish/crypto/foundation/nameservice"
+	"github.com/docker/docker/daemon/events"
 	"go.uber.org/zap"
 )
 
 // MuxConfig contains all the mandatory systems required by handlers
-// type MuxConfig struct {
-// 	Shutdown chan os.Signal
-// 	Log      *zap.SugaredLogger
-// 	State    *state.State
-// 	NS       *nameservice.NameService
-// 	Evts     *events.Events
-// }
+type MuxConfig struct {
+	Shutdown chan os.Signal
+	Log      *zap.SugaredLogger
+	State    *state.State
+	NS       *nameservice.NameService
+	Evts     *events.Events
+}
+
+// PublicMux constructs a http.Handler with all application routes defined.
+func PublicMux(cfg MuxConfig) http.Handler {
+
+	// Construct the web.App which holds all routes as well as common Middleware.
+	app := web.NewApp(
+		cfg.Shutdown,
+		mid.Logger(cfg.Log),
+		mid.Errors(cfg.Log),
+		mid.Metrics(),
+		mid.Cors("*"),
+		mid.Panics(),
+	)
+
+	// Accept CORS 'OPTIONS' preflight requests if config has been provided.
+	// Don't forget to apply the CORS middleware to the routes that need it.
+	// Example Config: `conf:"default:https://MY_DOMAIN.COM"`
+	h := func(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+		return nil
+	}
+	app.Handle(http.MethodOptions, "", "/*", h, mid.Cors("*"))
+
+	// Load the v1 routes.
+	v1.PublicRoutes(app, v1.Config{
+		Log:   cfg.Log,
+		State: cfg.State,
+		NS:    cfg.NS,
+		Evts:  cfg.Evts,
+	})
+
+	return app
+}
+
+// PrivateMux constructs a http.Handler with all application routes defined.
+func PrivateMux(cfg MuxConfig) http.Handler {
+
+	// Construct the web.App which holds all routes as well as common Middleware.
+	app := web.NewApp(
+		cfg.Shutdown,
+		mid.Logger(cfg.Log),
+		mid.Errors(cfg.Log),
+		mid.Metrics(),
+		mid.Cors("*"),
+		mid.Panics(),
+	)
+
+	// Accept CORS 'OPTIONS' preflight requests if config has been provided.
+	// Don't forget to apply the CORS middleware to the routes that need it.
+	// Example Config: `conf:"default:https://MY_DOMAIN.COM"`
+	h := func(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+		return nil
+	}
+	app.Handle(http.MethodOptions, "", "/*", h, mid.Cors("*"))
+
+	// Load the v1 routes.
+	v1.PrivateRoutes(app, v1.Config{
+		Log:   cfg.Log,
+		State: cfg.State,
+		NS:    cfg.NS,
+	})
+
+	return app
+}
 
 // DebugStandardLibraryMux registers all the debug routes from the standard library
 // into a new mux, bypassing the use of DefaultServerMux.
--- go.mod
+++ go.mod
@@ -4,10 +4,27 @@ go 1.17
 
 require (
 	github.com/ardanlabs/conf/v3 v3.1.2
+	github.com/docker/docker v1.4.2-0.20180625184442-8e610b2b55bf
+	github.com/ethereum/go-ethereum v1.10.17
 	go.uber.org/zap v1.21.0
 )
 
 require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/btcsuite/btcd/btcec/v2 v2.1.2 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
+	github.com/docker/distribution v2.8.1+incompatible // indirect
+	github.com/docker/go-metrics v0.0.1 // indirect
+	github.com/golang/protobuf v1.4.3 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/prometheus/client_golang v1.1.0 // indirect
+	github.com/prometheus/client_model v0.0.0-20190812154241-14fe0d1b01d4 // indirect
+	github.com/prometheus/common v0.6.0 // indirect
+	github.com/prometheus/procfs v0.0.3 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
+	golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2 // indirect
+	golang.org/x/sys v0.0.0-20210816183151-1e6c022a8912 // indirect
+	google.golang.org/protobuf v1.23.0 // indirect
 )
