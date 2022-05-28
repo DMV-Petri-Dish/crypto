d8beb5afeaa6af99f6e237c12ce1c7fd3db49dc7
john <jnkroeker@gmail.com>
2022-05-27 20:45:47 -0400

    complete wallet cli tool with cobra

---
+++ app/services/node/handlers/v1/v1.go
@@ -0,0 +1,50 @@
+// Package v1 contains the full set of handler functions and routes
+// supported by the v1 web api.
+package v1
+
+import (
+	"net/http"
+
+	"github.com/DMV-Petri-Dish/crypto/foundation/blockchain/state"
+	"github.com/DMV-Petri-Dish/crypto/foundation/events"
+	"github.com/DMV-Petri-Dish/crypto/foundation/nameservice"
+	"github.com/DMV-Petri-Dish/crypto/foundation/web"
+	"github.com/gorilla/websocket"
+	"go.uber.org/zap"
+)
+
+const version = "v1"
+
+// config contains all the mandatory systems required by handlers.
+type Config struct {
+	Log   *zap.SugaredLogger
+	State *state.State
+	NS    *nameservice.NameService
+	Evts  *events.Events
+}
+
+// PublicRoutes binds all the version 1 public routes.
+func PublicRoutes(app *web.App, cfg Config) {
+	pbl := public.Handlers{
+		Log:   cfg.Log,
+		State: cfg.State,
+		NS:    cfg.NS,
+		WS:    websocket.Upgrader{},
+		Evts:  cfg.Evts,
+	}
+
+	app.Handle(http.MethodGet, version, "/events", pbl.Events)
+	app.Handle(http.MethodGet, version, "/genesis/list", pbl.Genesis)
+
+}
+
+// PrivateRoutes binds all the version 1 private routes
+func PrivateRoutes(app *web.App, cfg Config) {
+	prv := private.Handlers{
+		Log:   cfg.Log,
+		State: cfg.State,
+		NS:    cfg.NS,
+	}
+
+	app.Handle(http.MethodPost, version, "/node/peers", prv.SubmitPeer)
+}
